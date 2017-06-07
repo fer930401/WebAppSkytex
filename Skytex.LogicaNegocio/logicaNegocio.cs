d5d0e313202e7e392406650d0615a2b5559beb37
Fernando Garcia <fer_930401@hotmail.com>
2017-06-07 17:15:04 -0500

    Logicanegocio
    Logicanegocio

---
+++ Skytex.LogicaNegocio/logicaNegocio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccesoDatos;
+
+namespace Skytex.LogicaNegocio
+{
+    public class logicaNegocio
+    {
+        AccesoDatos.operaciones datos;
+
+        public logicaNegocio()
+        {
+            datos = new operaciones();
+        }
+        public string numNomina(string num_nom, string rfc){
+            return datos.numNomina(num_nom, rfc);
+        }
+    }
+}
